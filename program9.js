dadec4c98bde4bde6554f6f1fc4e94f763637fb4
akki312 <akshithsistla@gmail.com>
2024-01-24 15:02:31 +0530

    program9.js

---
+++ program9.js
@@ -0,0 +1,9 @@
+let text = "";
+const today = new Date();
+const someday = new Date();
+someday.setfullyear(2100, 0 , 14);
+if (someday > today) {
+text = "today is before january 14, 2100.";
+} else {
+text = "today is before january 14, 2100.";
+}
