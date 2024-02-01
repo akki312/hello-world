c53149f9878a768679c666fc31561c8384333c86
akki312 <akshithsistla@gmail.com>
2024-02-01 16:16:39 +0530

    program33.js

---
+++ program33.js
@@ -0,0 +1,11 @@
+// short date format "MM/DD/YYYY"
+const date = new Date("03/25/2015");
+console.log(date); 
+const date1 = new Date("jul 1 2020");
+console.log(date1);
+const date2 = new Date("1 jul 2020");
+console.log(date2);
+const date3 = new Date("jul 1 2020");
+console.log(date3);
+const date4 = new Date("july, 1, 2020");
+console.log(date4);
