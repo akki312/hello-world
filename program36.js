65210a21502aa3e5d7e17cfcf47b72a53cebcae6
akki312 <akshithsistla@gmail.com>
2024-02-01 17:27:07 +0530

    program36.js

---
+++ program36.js
@@ -0,0 +1,10 @@
+// program to get the file extension
+function getFileExtension(filename){
+    // get file extension
+    const extension = filename.split('.').pop();
+    return extension;
+}
+const result1 = getFileExtension('module.js');
+console.log(result1);
+const result2 = getFileExtension('module.txt');
+console.log(result2);
