875363032f38390c7df8e621a4e522ad7e0065d2
akki312 <akshithsistla@gmail.com>
2024-02-06 15:33:12 +0530

    question5.js

---
+++ question5.js
@@ -0,0 +1,10 @@
+function printBox(width, height) {
+      let border = "*".repeat(width);
+    console.log(border);
+ for (let i = 0; i < height - 2; i++) {
+        let middle = "*" + " ".repeat(width - 2) + "*";
+        console.log(middle);
+        }
+   console.log(border);
+}
+printBox(20, 5);
