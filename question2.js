17796f6f55dc9b6ecf60b81af5e330a7caf8a40a
akki312 <akshithsistla@gmail.com>
2024-02-06 13:44:15 +0530

    question1.js

--- question2.js
+++ question2.js
@@ -1,10 +1,12 @@
 //*q1//
 let n= 5;
 let string = " ";
+let count = 1;
 for (i = 1;i <= n; i++){
     
     for (j = 1;j <= i; j++){
-        string += j;
+        string += count;
+        count ++;
 }
 string +="\n";
 }
