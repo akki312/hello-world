a7a132ded835324227286327a632f51af73661ea
akki312 <akshithsistla@gmail.com>
2024-02-08 18:56:37 +0530

    arraymethod3.js

--- arraymethod3.js
+++ arraymethod3.js
@@ -2,3 +2,21 @@ let number = [1, 2, 3];
 let otherNumbers = [4, 5, 6];
 let combinedArray = numbers.concat(otherNumbers);
 console.log("combined array:", combinedArray);
+
+let numbers = [1, 2, 3, 4, 5, 6];
+let slicedArray = numbers.slice(2, 4);
+console.loh("sliced array:", slicedArray);
+
+let numbers = [1, 2, 3, 4, 5];
+let renowed = numbers.splice(2, 2, 6, 7);
+console.log("array after splice():", numbers, "removed:",removedItems);
+ 
+let numbers = [1, 2, 3, 4, 5];
+console.log("index of '3':", number.indexof(3));
+console.log("last index of '3':", numbers.lastIndexOf(3));
+console.log("using foreach():");
+number.foreach(item => console.log(item));
+
+var str = "hello";
+var substr_expr = str.substr(6, 8);
+console.log(substr_expr);
