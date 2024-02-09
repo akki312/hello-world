deffed31ca97ed6b4ed967057d8dbc8715cf6384
akki312 <akshithsistla@gmail.com>
2024-02-09 11:33:34 +0530

    arrayexample1.js

--- arraymethod3.js
+++ arraymethod3.js
@@ -3,7 +3,7 @@ let otherNumbers = [4, 5, 6];
 let combinedArray = numbers.concat(otherNumbers);
 console.log("combined array:", combinedArray);
 
-let numbers = [1, 2, 3, 4, 5, 6];
+let number = [1, 2, 3, 4, 5, 6];
 let slicedArray = numbers.slice(2, 4);
 console.loh("sliced array:", slicedArray);
 
@@ -20,3 +20,74 @@ number.foreach(item => console.log(item));
 var str = "hello";
 var substr_expr = str.substr(6, 8);
 console.log(substr_expr);
+
+var str = "abcde";
+var char_at = str.charAt(6);
+console.log(char_at);
+
+var str = "abcde";
+var is_last = str.endsWith('sys');
+console.log(is_last);
+
+var str = "abcde";
+var is_last = str.endsWith('c');
+console.log(is_last);
+var is_last = str.includes('sys');
+console.log(is_incl);
+
+var is_incl = str.includes('xyz');
+console.log(is_incl);
+
+var index_char = str.indexOf('sys');
+console.log(index_char);
+
+var index_char = str.indexOf('xyz');
+console.log(index_char);
+
+var match_expr = str.match("sys");
+console.log(match_expr);
+
+var match_expr = str.match("xyz");
+console.log(match_expr);
+
+var repeat_expr = str.repeat(3);
+console.log(repeat_expr);
+
+var str = "xyz";
+var replace_expr = str.replace('hello', hai);
+console.log(replace_expr);
+
+var str = "xyz";
+var replace_expr = str.replace('hai', 'bye');
+console.log(replace_expr);
+
+var str = "abcdxyz";
+var replace_expr = str.replace('hello', 'hai');
+console.log(replace_expr);
+
+var str = "asfn";
+var slice_expr = str.slice('6', '11');
+console.log(slice_expr);
+
+var str = "abcdxyz";
+var start_expr = str.startsWith("he");
+console.log(start_expr);
+
+var str = "hello abcd";
+var start_expr = str.startsWith("cy");
+console.log(start_expr);
+
+var str = "abcdserga";
+var start_expr = str.startsWith("cy", 6);
+console.log(start_expr);
+
+var str = "aksg;jb";
+var substr_expr = str.substr(6, 8);
+console.log(substr_expr);
+
+var str = "agub';oisab";
+var substr_expr = str.substr(13, 5);
+console.log(substr_expr);
+
+
+
