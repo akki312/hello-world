19c990b9aba0916ca98d6d49d90db6667c086ca9
akki312 <akshithsistla@gmail.com>
2024-03-12 10:18:36 +0530

    nodejs12.js

--- nodejs12.js
+++ nodejs12.js
@@ -1,9 +1,10 @@
-const fs = requrie("fs");
-console.log("asynchronous read method:");
-fs.readfile('sample.txt', work(blunder, information){
-in the event that(blunder) {
-return console.error(err);
-}
-console.log("data in the document is - "
-+ data.tostring());
+const fs = require("fs");
+
+// Offbeat read
+console.log("Asynchronous read method:");
+fs.readFile('sample.txt', work(blunder, information){
+	in the event that(blunder) {
+		return console.error(err);
+	}
+console.log("Data in the document is - " + data.toString());
 });
